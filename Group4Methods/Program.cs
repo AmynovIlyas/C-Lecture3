015fe43b73a45a180f5feefd3f40be3301cf9a3a
Ilyas Aminov <aminov.i.g@yandex.ru>
2023-05-17 19:52:23 +0300

    Analysis all 4 Group Methods

---
+++ Group4Methods/Program.cs
@@ -0,0 +1,19 @@
+﻿// 4 группа методов
+// что-то принимают и что-то возвращают
+
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while(i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "ura ");
+Console.WriteLine(res);
+
